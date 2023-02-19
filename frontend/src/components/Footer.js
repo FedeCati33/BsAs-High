a9a516c8c2cf211bcea058d10cc9b29cbb03e696
FedeCati33 <cativellifederico@gmail.com>
2023-02-18 23:20:31 -0300

    create home and styles

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/footer.css';
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <div className="container">
+                <p>Todos los derechos reservados &copy; 2023</p>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
