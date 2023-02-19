c36cc4fcdfab6f05c26a1e4bdfc03480fc6c62f6
FedeCati33 <87648871+FedeCati33@users.noreply.github.com>
2023-02-18 23:22:01 -0300

    [FTR] Create mktplace (#2)
    * create components scaffolding
    
    * create home and styles

---
+++ frontend/src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/home.css';
+
+const Home = () => {
+    return (
+        <div className="home">
+            <h1>Bienvenido a mi Marketplace</h1>
+            <p>Encuentra los mejores productos al mejor precio</p>
+        </div>
+    );
+};
+
+export default Home;
---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+import "../styles/navbar.css";
+
+function Navbar() {
+    return (
+        <nav className="navbar">
+            <ul>
+                <li>
+                </li>
+                <li>
+                </li>
+                <li>
+                </li>
+                <li>
+                </li>
+                <li>
+                </li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Navbar;
